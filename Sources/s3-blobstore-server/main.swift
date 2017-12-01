80f7c80a99254e23ce618f5d5c0baf75cc13ae23
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-12-01 17:40:34 +0100

    Add todos

--- Sources/s3-blobstore-server/main.swift
+++ Sources/s3-blobstore-server/main.swift
@@ -4,6 +4,7 @@ import S3
 
 print("start S3 test upload")
 
+// FIX manage environment config
 let accessKey = ProcessInfo.processInfo.environment["AWS_ACCESS_KEY"]!
 let secretKey = ProcessInfo.processInfo.environment["AWS_SECRET_KEY"]!
 let bucket = ProcessInfo.processInfo.environment["AWS_BUCKET"]!
@@ -14,14 +15,13 @@ let projectHome = ProcessInfo.processInfo.environment["PROJECT_HOME"]!
 print("Upload to: \(host)")
 print("Varibales are set")
 
+// FIX seperate make bytes
 let fm = FileManager()
 let textBlob = fm.contents(atPath: projectHome.appending("/testblob.txt"))
 print(textBlob?.makeString() as Any)
 let bytes = textBlob?.makeBytes()
 
-
-
-
+// TODO refactore the upload
 try S3(host: host, accessKey: accessKey, secretKey: secretKey, region: Region.euWest1).upload(bytes: bytes!, path: "/swift/s3/test-bucket", access: AccessControlList.privateAccess)
 
 print("Upload done 😀")
