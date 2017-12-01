3874ea22b8bd44119b708b30ea7fedb18c83c78f
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-12-01 17:36:58 +0100

    Implement the upload example for S3

--- Sources/s3-blobstore-server/main.swift
+++ Sources/s3-blobstore-server/main.swift
@@ -1,6 +1,27 @@
 import Foundation
+import AWSSignatureV4
+import S3
 
-// fetch env
+print("start S3 test upload")
 
-print("Hello, world!")
+let accessKey = ProcessInfo.processInfo.environment["AWS_ACCESS_KEY"]!
+let secretKey = ProcessInfo.processInfo.environment["AWS_SECRET_KEY"]!
+let bucket = ProcessInfo.processInfo.environment["AWS_BUCKET"]!
+let host = bucket.appending(".s3.amazonaws.com")
 
+let projectHome = ProcessInfo.processInfo.environment["PROJECT_HOME"]!
+
+print("Upload to: \(host)")
+print("Varibales are set")
+
+let fm = FileManager()
+let textBlob = fm.contents(atPath: projectHome.appending("/testblob.txt"))
+print(textBlob?.makeString() as Any)
+let bytes = textBlob?.makeBytes()
+
+
+
+
+try S3(host: host, accessKey: accessKey, secretKey: secretKey, region: Region.euWest1).upload(bytes: bytes!, path: "/swift/s3/test-bucket", access: AccessControlList.privateAccess)
+
+print("Upload done 😀")
